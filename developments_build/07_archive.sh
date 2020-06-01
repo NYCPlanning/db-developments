10c4e4b67e33ead3ee0f16e6e62161f1eaa7c427
sptkl <caobaiyue@gmail.com>
2020-06-01 14:00:58 +0000

    create bash functions to simplify processes
    Former-commit-id: c728f6d3f171cfcca6cc7f56b240f6853e6554a5

--- developments_build/07_archive.sh
+++ developments_build/07_archive.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 source config.sh
-DATE=$(date "+%Y/%m/%d");
+
+function archive {
+    echo "archiving $1 -> $2"
+    pg_dump -t $1 $BUILD_ENGINE -O -c | psql $EDM_DATA
+    psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS $2;";
+    psql $EDM_DATA -c "ALTER TABLE $1 SET SCHEMA $2;";
+    psql $EDM_DATA -c "DROP VIEW IF EXISTS $2.latest;";
+    psql $EDM_DATA -c "DROP TABLE IF EXISTS $2.\"$DATE\";";
+    psql $EDM_DATA -c "ALTER TABLE $2.$1 RENAME TO \"$DATE\";";
+    psql $EDM_DATA -c "CREATE VIEW $2.latest AS (SELECT '$DATE' as v, * FROM $2.\"$DATE\");"
+}
 
 # archive developments DB
-pg_dump -t devdb_export --no-owner $BUILD_ENGINE | psql $EDM_DATA
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS developments;";
-psql $EDM_DATA -c "ALTER TABLE devdb_export SET SCHEMA developments;";
-psql $EDM_DATA -c "DROP VIEW IF EXISTS developments.latest;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS developments.\"$DATE\";";
-psql $EDM_DATA -c "ALTER TABLE developments.devdb_export RENAME TO \"$DATE\";";
-psql $EDM_DATA -c "CREATE VIEW developments.latest AS (SELECT '$DATE' as v, * FROM developments.\"$DATE\");"
+archive devdb_export developments
 
 # archive housing DB
-pg_dump -t housing_export --no-owner $BUILD_ENGINE | psql $EDM_DATA
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_housing;";
-psql $EDM_DATA -c "ALTER TABLE housing_export SET SCHEMA dcp_housing;";
-psql $EDM_DATA -c "DROP VIEW IF EXISTS dcp_housing.latest;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_housing.\"$DATE\";";
-psql $EDM_DATA -c "ALTER TABLE dcp_housing.housing_export RENAME TO \"$DATE\";";
-psql $EDM_DATA -c "CREATE VIEW dcp_housing.latest AS (SELECT '$DATE' as v, * FROM dcp_housing.\"$DATE\");"
+archive housing_export dcp_housing
 
 # archive yearly_unitchange table
-pg_dump -t yearly_unitchange --no-owner $BUILD_ENGINE | psql $EDM_DATA
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS yearly_unitchange;";
-psql $EDM_DATA -c "ALTER TABLE yearly_unitchange SET SCHEMA yearly_unitchange;";
-psql $EDM_DATA -c "DROP VIEW IF EXISTS yearly_unitchange.latest;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS yearly_unitchange.\"$DATE\";";
-psql $EDM_DATA -c "ALTER TABLE yearly_unitchange.yearly_unitchange RENAME TO \"$DATE\";";
-psql $EDM_DATA -c "CREATE VIEW yearly_unitchange.latest AS (SELECT '$DATE' as v, * FROM yearly_unitchange.\"$DATE\");"
+archive yearly_unitchange yearly_unitchange
