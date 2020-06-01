c728f6d3f171cfcca6cc7f56b240f6853e6554a5
sptkl <caobaiyue@gmail.com>
2020-06-01 14:00:58 +0000

    create bash functions to simplify processes

--- developments_build/06_qaqc.sh
+++ developments_build/06_qaqc.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source config.sh
-source ./url_parse.sh $BUILD_ENGINE
 
-DATE=$(date "+%Y-%m-%d")
 mkdir -p output
 
 echo "Exporting QAQC scripts"
@@ -11,45 +9,57 @@ psql $BUILD_ENGINE -f sql/qc_geom.sql
 psql $BUILD_ENGINE -f sql/qc_null.sql
 psql $BUILD_ENGINE -f sql/qc_mismatch.sql
 psql $BUILD_ENGINE -f sql/qc_completeness.sql
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_jobtypestats) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_jobtypestats.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_countsstats) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_countsstats.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_units_complete_stats) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_units_complete_stats.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_geocodedstats) TO  stdout DELIMITER ',' CSV HEADER;" > output/qc_geocodedstats.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_a1_units_prop) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_a1_units_prop.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_a1_units_init) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_a1_units_init.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_hotel_resid_nb_a1) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_hotel_resid_nb_a1.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_hotel_resid_a1) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_hotel_resid_a1.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_nonresid_large) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_nonresid_large.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_potentialdups) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_potentialdups.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_occupancyresearch) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_occupancyresearch.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_hny_mismatch) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_hny_mismatch.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_unclipped) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_unclipped.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_cofos_units) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_cofos_units.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_mismatch_complete_units) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_mismatch_complete_units.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_mismatch_incomplete_units) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_mismatch_incomplete_units.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_sro) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_sro.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_jobnum) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_jobnum.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_geom) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_geom.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_null) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_null.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_completeness) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_completeness.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_mismatch) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_mismatch.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM dev_qc_geo_mismatch) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_geo_mismatch.csv
 
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_outliers
-                                WHERE job_number NOT IN (
-                                    SELECT DISTINCT job_number
-                                    FROM qc_outliersacrhived
-                                    WHERE outlier = 'N' OR outlier = 'C')
-                                    AND job_number NOT IN (
-                                        SELECT DISTINCT job_number
-                                        FROM developments
-                                        WHERE x_dcpedited = 'true')) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_outliers.csv
-psql $BUILD_ENGINE -c "\COPY (SELECT * FROM qc_outliersacrhived) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_outliersacrhived.csv
+function export_csv {
+    psql $BUILD_ENGINE -c "\COPY (
+        SELECT * FROM $1
+    ) TO stdout DELIMITER ',' CSV HEADER;" > output/$1.csv
 
+}
+
+export_csv dev_qc_jobtypestats &
+export_csv dev_qc_countsstats &
+export_csv dev_qc_units_complete_stats &
+export_csv dev_qc_geocodedstats &
+export_csv dev_qc_a1_units_prop &
+export_csv dev_qc_a1_units_init &
+export_csv dev_qc_hotel_resid_nb_a1 &
+export_csv dev_qc_hotel_resid_a1 &
+export_csv dev_qc_nonresid_large &
+export_csv dev_qc_potentialdups &
+export_csv dev_qc_occupancyresearch &
+export_csv dev_qc_hny_mismatch &
+export_csv dev_qc_unclipped &
+export_csv dev_qc_cofos_units &
+export_csv dev_qc_mismatch_complete_units &
+export_csv dev_qc_mismatch_incomplete_units &
+export_csv dev_qc_sro &
+export_csv dev_qc_jobnum &
+export_csv qc_geom &
+export_csv qc_null &
+export_csv qc_completeness &
+export_csv qc_mismatch &
+export_csv dev_qc_geo_mismatch &
+export_csv qc_outliersacrhived &
+
+psql $BUILD_ENGINE -c "\COPY (
+    SELECT * FROM qc_outliers
+    WHERE job_number NOT IN (
+        SELECT DISTINCT job_number
+        FROM qc_outliersacrhived
+        WHERE outlier = 'N' OR outlier = 'C')
+        AND job_number NOT IN (
+            SELECT DISTINCT job_number
+            FROM developments
+            WHERE x_dcpedited = 'true')
+) TO stdout DELIMITER ',' CSV HEADER;" > output/qc_outliers.csv
+
+wait
 # output the aggregation table by census tract
 psql $BUILD_ENGINE -f sql/qc_ct.sql
 
 # qc_ct
+urlparse $BUILD_ENGINE
 mkdir -p output/qc_ct && 
     (cd output/qc_ct
         pgsql2shp -u $BUILD_USER -h $BUILD_HOST -p $BUILD_PORT -P $BUILD_PWD -f qc_ct $BUILD_DB \
--- developments_build/config.sh
+++ developments_build/config.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
-if [ -f version.env ]
-then
-  export $(cat version.env | sed 's/#.*//g' | xargs)
-fi
+#!/bin/bash
+
+function set_env {
+  for envfile in $@
+  do
+    if [ -f $envfile ]
+      then
+        export $(cat $envfile | sed 's/#.*//g' | xargs)
+      fi
+  done
+}
+
+function urlparse {
+    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    url=$(echo $1 | sed -e s,$proto,,g)
+    userpass="$(echo $url | grep @ | cut -d@ -f1)"
+    BUILD_PWD=`echo $userpass | grep : | cut -d: -f2`
+    BUILD_USER=`echo $userpass | grep : | cut -d: -f1`
+    hostport=$(echo $url | sed -e s,$userpass@,,g | cut -d/ -f1)
+    BUILD_HOST="$(echo $hostport | sed -e 's,:.*,,g')"
+    BUILD_PORT="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+    BUILD_DB="$(echo $url | grep / | cut -d/ -f2-)"
+}
+
+# Setting Environmental Variables
+set_env .env version.env
+DATE=$(date "+%Y-%m-%d")
+
