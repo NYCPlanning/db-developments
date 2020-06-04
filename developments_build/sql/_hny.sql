9f44e68e4e968e6b82b60db8c5df5e3752a26752
mgraber <molly.graber@me.com>
2020-06-04 15:59:23 +0000

    Identify many-to-one and one-to-many

--- developments_build/sql/_hny.sql
+++ developments_build/sql/_hny.sql
@@ -2,7 +2,11 @@
     Geocoding-derived BIN, BBL, and geometry are what we use
     to match a hny records with a developments record. In this step,
     we also create a hash unique ID and a geometry object from
-    the coordinates **/
+    the coordinates.
+    
+    We will only look at hny records where the name indicates that 
+    it is not confidential, and where the building is new construction **/
+
 WITH hny AS (
         SELECT md5(CAST((a.project_id,
                        a.number,
@@ -10,7 +14,7 @@ WITH hny AS (
                        b.geo_bin,
                        b.geo_bbl) AS text)) as hny_id,
                 a.*, b.geo_bbl, b.geo_bin, b.geo_latitude, b.geo_longitude,
-                (CASE WHEN b.geo_longitude != '' AND b.geo_latitude != ''
+                (CASE WHEN b.geo_longitude IS NOT NULL AND b.geo_latitude IS NOT NULL
                     THEN ST_SetSRID(ST_MakePoint(b.geo_longitude::NUMERIC, 
                                     b.geo_latitude::NUMERIC),4326)
                     ELSE NULL
@@ -51,8 +55,8 @@ WITH hny AS (
         ON h.geo_bbl = d.geo_bbl 
             AND h.geo_bin = d.geo_bin
             AND ABS(h.total_units::NUMERIC - d.units_prop::NUMERIC) <=5
-        AND h.geo_bin !='' AND h.geo_bin IS NOT NULL
-        AND h.geo_bbl !='' AND h.geo_bbl IS NOT NULL
+        AND h.geo_bin IS NOT NULL
+        AND h.geo_bbl IS NOT NULL
         AND d.status <> 'Withdrawn'
         AND d.job_type <> 'Demolition'
     ),
@@ -68,10 +72,9 @@ WITH hny AS (
         FROM hny h
         JOIN developments_hny d
         ON h.geo_bbl = d.geo_bbl 
-            AND h.geo_bin <> d.geo_bin
+            AND (h.geo_bin <> d.geo_bin OR h.geo_bin IS NULL OR d.geo_bin IS NULL)
             AND ABS(h.total_units::NUMERIC - d.units_prop::NUMERIC) <=5
-        AND h.geo_bin !='' AND h.geo_bin IS NOT NULL
-        AND h.geo_bbl !='' AND h.geo_bbl IS NOT NULL
+        AND h.geo_bbl IS NOT NULL
         AND d.status <> 'Withdrawn'
         AND d.job_type <> 'Demolition'
     ),
@@ -87,8 +90,8 @@ WITH hny AS (
         FROM hny h
         JOIN developments_hny d
         ON ST_DWithin(h.geom::geography, d.geom::geography, 5)
-            AND h.geo_bbl <> d.geo_bbl 
-            AND h.geo_bin <> d.geo_bin
+            AND (h.geo_bbl <> d.geo_bbl OR h.geo_bbl IS NULL OR d.geo_bbl IS NULL)
+            AND (h.geo_bin <> d.geo_bin OR h.geo_bin IS NULL OR d.geo_bin IS NULL)
             AND ABS(h.total_units::NUMERIC - d.units_prop::NUMERIC) <=5
         AND h.geom IS NOT NULL AND d.geom IS NOT NULL
         AND d.status <> 'Withdrawn'
@@ -130,8 +133,8 @@ WITH hny AS (
 		        UNION 
 		        SELECT * FROM bbl_match 
 		        UNION 
-		        SELECT * FROM spatial_match) a) 
-    )  
+		        SELECT * FROM spatial_match) a), 
+  
 	-- For each hny record, find the highest-priority match(es)
 	best_matches AS (SELECT t1.hny_id, t1.match_priority, 
 							t2.job_number, t2.job_type, 
@@ -143,9 +146,31 @@ WITH hny AS (
 					) AS t1 
 					RIGHT JOIN all_matches AS t2 
 					ON t2.hny_id = t1.hny_id 
-					AND t2.match_priority = t1.match_priority)
-SELECT *
-FROM best_matches;
+					AND t2.match_priority = t1.match_priority),
+                    
+	-- Find many-hny-to-one-developments
+	many_developments AS (SELECT hny_id, count(*)
+				FROM best_matches
+				GROUP BY hny_id),
+				
+	-- Find many-developments-to-one-hny
+	many_hny AS (SELECT job_number, count(*)
+				FROM best_matches
+				GROUP BY job_number)	
+
+	-- Add relationship flags
+	SELECT bm.*,
+		(CASE 
+			WHEN hny_id IN (SELECT DISTINCT hny_id FROM many_developments
+									WHERE count > 1) THEN 1
+			ELSE 0 
+		END) AS one_hny_to_many_dev,
+		(CASE 
+			WHEN job_number IN (SELECT DISTINCT job_number FROM many_hny
+									WHERE count > 1) THEN 1
+			ELSE 0
+		END) AS one_dev_to_many_hny
+	FROM best_matches bm;
 
 -- Logic to assign matches
 
