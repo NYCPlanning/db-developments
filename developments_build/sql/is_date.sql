0e3aa9f16315f2513bb23514012a96f97d117c12
sptkl <caobaiyue@gmail.com>
2020-06-12 20:21:44 +0000

    move is_date.sql to bash script

--- developments_build/sql/is_date.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION is_date(s varchar) RETURNS boolean AS $$
-BEGIN
-  perform s::date;
-  RETURN true;
-exception WHEN others THEN
-  RETURN false;
-END;
-$$ LANGUAGE plpgsql;
