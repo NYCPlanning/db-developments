f543c9be8b1e4fc2985f6d6a2c656bebd7ab9793
mgraber <molly.graber@me.com>
2020-06-16 16:00:51 +0000

    Add occ qaqc

---
+++ developments_build/sql/qaqc/qaqc_occ.sql
@@ -0,0 +1,18 @@
+/** QAQC
+	b_nonres_with_units
+	units_res_accessory
+	b_likely_occ_desc
+**/
+
+DROP TABLE IF EXISTS OCC_qaqc;
+WITH 
+JOBNUMBER_nonres_units AS (
+
+),
+JOBNUMBER_accessory AS (
+
+),
+JOBNUMBER_b_likely AS (
+
+)
+
