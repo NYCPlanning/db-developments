c93c7caa96cd57f0610a90dee16ef008a1e505f2
mgraber <molly.graber@me.com>
2020-06-17 14:30:01 +0000

    Add flag for dem and nb sharing bin and bbl
    Former-commit-id: 1333c65ec4de61e6b07f891b99fbc894bee33cf0

--- developments_build/sql/qaqc/qaqc_mid.sql
+++ developments_build/sql/qaqc/qaqc_mid.sql
@@ -1,4 +1,6 @@
 /** QAQC
+	JOB_TYPE:
+		dem_nb_overlap
     UNITS:
         units_init_null
 	    units_init_null
@@ -21,6 +23,7 @@ JOBNUMBER_all AS(
 	SELECT DISTINCT job_number
 	FROM MID_devdb
 ),
+
 
 JOBNUMBER_null_init AS(
     SELECT job_number
@@ -75,6 +78,23 @@ MATCHES_dup_diff_equal_units AS (
     ON a.job_number = b.job_number
 ),
 
+JOBNUMBER_dem_nb_overlap AS (
+    SELECT a.job_number, 
+    	b.job_number as dem_nb_overlap
+    FROM MID_devdb a 
+    JOIN MID_devdb b 
+    ON a.bbl = b.bbl
+    AND a.bin = b.bin
+    AND a.job_type = 'Demolition'
+    AND b.job_type = 'New Building'
+),
+
+MATCHES_dem_nb_overlap AS (
+    SELECT a.job_number, b.dem_nb_overlap
+    FROM JOBNUMBER_all a
+    LEFT JOIN JOBNUMBER_dem_nb_overlap b
+    ON a.job_number = b.job_number
+),
 
 JOBNUMBER_nonres_units AS (
 	SELECT job_number 
@@ -124,42 +144,48 @@ JOBNUMBER_incomplete_tract AS (
     FROM MID_devdb
     WHERE tracthomes = 'Y'
     AND job_status LIKE 'Complete'
-)
+),
 
+_MID_qaqc AS (
+	SELECT a.*,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_init) THEN 1
+		 	ELSE 0
+		END) as units_init_null,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_prop) THEN 1
+		 	ELSE 0
+		END) as units_prop_null,
+	    b.dup_equal_units,
+	    b.dup_diff_units,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_nonres_units) THEN 1
+		 	ELSE 0
+		END) as b_nonres_with_units,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_accessory) THEN 1
+		 	ELSE 0
+		END) as units_res_accessory,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_b_likely) THEN 1
+		 	ELSE 0
+		END) as b_likely_occ_desc,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
+		 	ELSE 0
+		END) as units_co_prop_mismatch,
+	    (CASE 
+		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
+		 	ELSE 0
+		END) as z_incomp_tract_home
+	
+	FROM STATUS_qaqc a
+	JOIN MATCHES_dup_diff_equal_units b
+	ON a.job_number = b.job_number)
+	
 SELECT a.*,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_init) THEN 1
-	 	ELSE 0
-	END) as units_init_null,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_prop) THEN 1
-	 	ELSE 0
-	END) as units_prop_null,
-    b.dup_equal_units,
-    b.dup_diff_units,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_nonres_units) THEN 1
-	 	ELSE 0
-	END) as b_nonres_with_units,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_accessory) THEN 1
-	 	ELSE 0
-	END) as units_res_accessory,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_b_likely) THEN 1
-	 	ELSE 0
-	END) as b_likely_occ_desc,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
-	 	ELSE 0
-	END) as units_co_prop_mismatch,
-    (CASE 
-	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
-	 	ELSE 0
-	END) as z_incomp_tract_home
-
-    
-INTO MID_devdb
-FROM STATUS_qaqc a
-JOIN MATCHES_dup_diff_equal_units b
+	b.dem_nb_overlap
+INTO MID_qaqc
+FROM _MID_qaqc a
+JOIN MATCHES_dem_nb_overlap b
 ON a.job_number = b.job_number;
