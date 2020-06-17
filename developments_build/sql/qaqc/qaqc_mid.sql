31fa9fb820a44cfb7a6b4e7c003fc4827d224332
mgraber <molly.graber@me.com>
2020-06-17 18:22:01 +0000

    Switch back to bool and create table of matches
    Former-commit-id: 63cd6cc7119866fce2f231e82c8e9f9f8e1bdebf

--- developments_build/sql/qaqc/qaqc_mid.sql
+++ developments_build/sql/qaqc/qaqc_mid.sql
@@ -15,6 +15,58 @@
     STATUS:
         z_incomp_tract_home
 **/
+DROP TABLE IF EXISTS MATCHES_dup_diff_equal_units;
+WITH
+JOBNUMBER_dup_equal_units AS (
+    SELECT a.job_number, b.job_number as dup_equal_units
+    FROM MID_devdb a 
+    JOIN MID_devdb b 
+    ON a.job_type = b.job_type
+    AND a.bbl = b.bbl
+    AND a.address = b.address
+    AND a.classa_net = b.classa_net
+    AND a.job_number <> b.job_number
+),
+
+JOBNUMBER_dup_diff_units AS (
+    SELECT a.job_number, b.job_number as dup_diff_units
+    FROM MID_devdb a 
+    JOIN MID_devdb b 
+    ON a.job_type = b.job_type
+    AND a.bbl = b.bbl
+    AND a.address = b.address
+    AND a.classa_net <> b.classa_net
+    AND a.job_number <> b.job_number
+),
+
+
+MATCHES_dup_diff_equal_units AS (
+    SELECT a.job_number, a.dup_equal_units, b.dup_diff_units
+    FROM JOBNUMBER_dup_equal_units a
+    FULL OUTER JOIN JOBNUMBER_dup_diff_units b
+    ON a.job_number = b.job_number
+)
+
+SELECT *
+INTO DUP_diff_job_number
+FROM MATCHES_dup_diff_equal_units;
+
+DROP TABLE IF EXISTS MATCH_dem_nb;
+WITH
+JOBNUMBER_dem_nb_overlap AS (
+    SELECT a.job_number as dem_job_number, 
+    	b.job_number as nb_job_number
+    FROM MID_devdb a
+	JOIN MID_devdb b 
+	ON a.geo_bbl = b.geo_bbl
+	WHERE a.job_type = 'Demolition'
+	AND b.job_type = 'New Building'
+)
+
+SELECT *
+INTO MATCH_dem_nb
+FROM JOBNUMBER_dem_nb_overlap;
+
 
 DROP TABLE IF EXISTS MID_qaqc;
 WITH
@@ -40,60 +92,6 @@ JOBNUMBER_null_prop AS(
     job_type IN ('New Building' , 'Alteration') 
     AND resid_flag = 'Residential' 
     AND classa_prop IS NULL),   
-
-JOBNUMBER_dup_equal_units AS (
-    SELECT a.job_number, b.job_number as dup_equal_units
-    FROM MID_devdb a 
-    JOIN MID_devdb b 
-    ON a.job_type = b.job_type
-    AND a.bbl = b.bbl
-    AND a.address = b.address
-    AND a.classa_net = b.classa_net
-    AND a.job_number <> b.job_number
-),
-
-JOBNUMBER_dup_diff_units AS (
-    SELECT a.job_number, b.job_number as dup_diff_units
-    FROM MID_devdb a 
-    JOIN MID_devdb b 
-    ON a.job_type = b.job_type
-    AND a.bbl = b.bbl
-    AND a.address = b.address
-    AND a.classa_net <> b.classa_net
-    AND a.job_number <> b.job_number
-),
-
-MATCHES_dup_equal_units AS (
-    SELECT a.job_number, b.dup_equal_units
-    FROM JOBNUMBER_all a
-    LEFT JOIN JOBNUMBER_dup_equal_units b
-    ON a.job_number = b.job_number
-),
-
-
-MATCHES_dup_diff_equal_units AS (
-    SELECT a.job_number, a.dup_equal_units, b.dup_diff_units
-    FROM MATCHES_dup_equal_units a
-    LEFT JOIN JOBNUMBER_dup_diff_units b
-    ON a.job_number = b.job_number
-),
-
-JOBNUMBER_dem_nb_overlap AS (
-    SELECT a.job_number, 
-    	b.job_number as dem_nb_overlap
-    FROM MID_devdb a
-	JOIN MID_devdb b 
-	ON a.geo_bbl = b.geo_bbl
-	WHERE a.job_type = 'Demolition'
-	AND b.job_type = 'New Building'
-),
-
-MATCHES_dem_nb_overlap AS (
-    SELECT a.job_number, b.dem_nb_overlap
-    FROM JOBNUMBER_all a
-    LEFT JOIN JOBNUMBER_dem_nb_overlap b
-    ON a.job_number = b.job_number
-),
 
 JOBNUMBER_nonres_units AS (
 	SELECT job_number 
@@ -143,48 +141,50 @@ JOBNUMBER_incomplete_tract AS (
     FROM MID_devdb
     WHERE tracthomes = 'Y'
     AND job_status LIKE 'Complete'
-),
+)
 
-_MID_qaqc AS (
-	SELECT a.*,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_init) THEN 1
-		 	ELSE 0
-		END) as units_init_null,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_prop) THEN 1
-		 	ELSE 0
-		END) as units_prop_null,
-	    b.dup_equal_units,
-	    b.dup_diff_units,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_nonres_units) THEN 1
-		 	ELSE 0
-		END) as b_nonres_with_units,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_accessory) THEN 1
-		 	ELSE 0
-		END) as units_res_accessory,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_b_likely) THEN 1
-		 	ELSE 0
-		END) as b_likely_occ_desc,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
-		 	ELSE 0
-		END) as units_co_prop_mismatch,
-	    (CASE 
-		 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
-		 	ELSE 0
-		END) as z_incomp_tract_home
-	
-	FROM STATUS_qaqc a
-	JOIN MATCHES_dup_diff_equal_units b
-	ON a.job_number = b.job_number)
-	
 SELECT a.*,
-	b.dem_nb_overlap
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_init) THEN 1
+	 	ELSE 0
+	END) as units_init_null,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_prop) THEN 1
+	 	ELSE 0
+	END) as units_prop_null,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM DUP_diff_job_number WHERE dup_equal_units IS NOT NULL) THEN 1
+	 	ELSE 0
+	END) as dup_equal_units,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM DUP_diff_job_number WHERE dup_diff_units IS NOT NULL) THEN 1
+	 	ELSE 0
+	END) as dup_diff_units,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_nonres_units) THEN 1
+	 	ELSE 0
+	END) as b_nonres_with_units,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_accessory) THEN 1
+	 	ELSE 0
+	END) as units_res_accessory,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_b_likely) THEN 1
+	 	ELSE 0
+	END) as b_likely_occ_desc,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
+	 	ELSE 0
+	END) as units_co_prop_mismatch,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) THEN 1
+	 	ELSE 0
+	END) as z_incomp_tract_home,
+	(CASE 
+	 	WHEN a.job_number IN (SELECT dem_job_number FROM MATCH_dem_nb) THEN 1
+	 	WHEN a.job_number IN (SELECT nb_job_number FROM MATCH_dem_nb) THEN 1
+	 	ELSE 0
+	END) as dem_nb_overlap
+	
 INTO MID_qaqc
-FROM _MID_qaqc a
-JOIN MATCHES_dem_nb_overlap b
-ON a.job_number = b.job_number;
+FROM STATUS_qaqc a;
