37229cef57fe408267b3151227462135617e9505
mgraber <molly.graber@me.com>
2020-06-19 19:48:38 +0000

    Use classa_net and date_complete for all fields
    Former-commit-id: c2e1b0932787e7602533b5e5ffe1fa11226cec77

--- developments_build/sql/yearly.sql
+++ developments_build/sql/yearly.sql
@@ -10,7 +10,7 @@ INPUTS:
         date_permittd,
         date_complete,
         date_lastupdt,
-        classa_complt,
+        classa_net,
         boro,
         bctcb2010,
         bct2010,
@@ -42,6 +42,11 @@ OUTPUTS:
         comp2019,
         comp2020,
         comp2020q2,
+        incmpfiled,
+        incmpprgrs,
+        incmprmtd,
+        incmpwtdrn,
+        inactive
     )
 
 */
@@ -58,16 +63,44 @@ DATES_complete_jobs AS (
         nta2010,
         ntaname2010,
         puma2010,
-        CASE WHEN job_type = 'Demolition' 
-            AND date_permittd::date IS NOT NULL
-            THEN date_permittd::date
-            WHEN job_type <> 'Demolition'
-            AND date_complete::date IS NOT NULL
-            THEN date_complete::date
-            ELSE date_lastupdt::date END as reference_date,
-        classa_complt
+        CASE WHEN job_status = '5. Complete' 
+                THEN date_complete::date
+            ELSE NULL END as reference_date,
+        classa_net
         FROM FINAL_devdb
-        WHERE job_status = '5. Complete'
+),
+
+INCOMPLETE_jobs AS (
+    SELECT
+        job_number,
+        CASE WHEN job_status LIKE '1%'
+                AND job_inactive IS NULL
+            THEN  classa_net 
+            ELSE NULL END as incmpfiled, 
+        CASE WHEN job_status = '2%'
+                AND job_inactive IS NULL
+            THEN  classa_net 
+            ELSE NULL END as incmpprgrs, 
+        CASE WHEN job_status = '3%'
+                AND job_inactive IS NULL
+            THEN  classa_net 
+            ELSE NULL END as incmprmtd, 
+        CASE WHEN job_status = '9%'
+                AND job_inactive IS NULL
+            THEN  classa_net 
+            ELSE NULL END as incmpwtdrn, 
+        CASE WHEN job_status <> '9. Withdrawn'
+                AND job_inactive = 'Inactive'
+            THEN  classa_net 
+            ELSE NULL END as inactive
+    FROM FINAL_devdb
+),
+
+DATES_join AS (
+    SELECT a.*, b.incmpfiled, b.incmpprgrs, b.incmprmtd, b.incmpwtdrn, b.inactive
+    FROM DATES_complete_jobs a 
+    JOIN INCOMPLETE_jobs b 
+    ON a.job_number = b.job_number
 )
 
 SELECT job_number,
@@ -79,54 +112,59 @@ SELECT job_number,
         puma2010,
         CASE WHEN reference_date > '2010-03-31'::date
             AND reference_date < '2011-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2010ap,
         CASE WHEN reference_date > '2009-12-31'::date
             AND reference_date < '2010-04-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2010,
         CASE WHEN reference_date > '2010-12-31'::date
             AND reference_date < '2012-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2011,
         CASE WHEN reference_date > '2011-12-31'::date
             AND reference_date < '2013-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2012,
         CASE WHEN reference_date > '2012-12-31'::date
             AND reference_date < '2014-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2013,
         CASE WHEN reference_date > '2013-12-31'::date
             AND reference_date < '2015-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2014,
         CASE WHEN reference_date > '2014-12-31'::date
             AND reference_date < '2016-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2015,
         CASE WHEN reference_date > '2015-12-31'::date
             AND reference_date < '2017-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2016,
         CASE WHEN reference_date > '2016-12-31'::date
             AND reference_date < '2018-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2017,
         CASE WHEN reference_date > '2017-12-31'::date
             AND reference_date < '2019-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2018,
         CASE WHEN reference_date > '2018-12-31'::date
             AND reference_date < '2020-01-01'::date
-            THEN classa_complt
+            THEN classa_net
             ELSE NULL END AS comp2019,
         CASE WHEN reference_date > '2019-12-31'::date
             AND reference_date < '2020-07-01'::date
-            THEN classa_complt
-            ELSE NULL END AS comp2020q
+            THEN classa_net
+            ELSE NULL END AS comp2020q2,
+        incmpfiled,
+        incmpprgrs,
+        incmprmtd,
+        incmpwtdrn,
+        inactive
 INTO YEARLY_devdb
-FROM DATES_complete_jobs;
+FROM DATES_join;
 
     
 
