6aebd0ec3dd85ffb615cfdaacf778022a6a179dd
mgraber <molly.graber@me.com>
2020-06-24 14:53:15 +0000

    Add fips codes
    Former-commit-id: b2b0b8475c51f26eacec75c05769028688ba32a0

--- developments_build/sql/yearly.sql
+++ developments_build/sql/yearly.sql
@@ -53,68 +53,72 @@ OUTPUTS:
 
 
 DROP TABLE IF EXISTS YEARLY_devdb;
-SELECT job_number,
-        boro,
-        bctcb2010,
-        bct2010,
-        nta2010,
-        ntaname2010,
-        puma2010,
-        CASE WHEN complete_year = '2010' AND date_complete > '2010-03-31'::date
-            THEN classa_net
+SELECT a.job_number,
+        a.boro,
+        a.bctcb2010,
+        b.fips_boro||RIGHT(a.bctcb2010, 10) as cenblock10,
+        a.bct2010,
+        b.fips_boro||RIGHT(a.bct2010, 6) as centract10,
+        a.nta2010,
+        a.ntaname2010,
+        a.puma2010,
+        CASE WHEN a.complete_year = '2010' AND a.date_complete > '2010-03-31'::date
+            THEN a.classa_net
             ELSE NULL END AS comp2010ap,
-        CASE WHEN complete_year = '2010' 
-            THEN classa_net
+        CASE WHEN a.complete_year = '2010' 
+            THEN a.classa_net
             ELSE NULL END AS comp2010,
-        CASE WHEN complete_year = '2011'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2011'
+            THEN a.classa_net
             ELSE NULL END AS comp2011,
-        CASE WHEN complete_year = '2012'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2012'
+            THEN a.classa_net
             ELSE NULL END AS comp2012,
-        CASE WHEN complete_year = '2013'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2013'
+            THEN a.classa_net
             ELSE NULL END AS comp2013,
-        CASE WHEN complete_year = '2014'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2014'
+            THEN a.classa_net
             ELSE NULL END AS comp2014,
-        CASE WHEN complete_year = '2015'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2015'
+            THEN a.classa_net
             ELSE NULL END AS comp2015,
-        CASE WHEN complete_year = '2016'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2016'
+            THEN a.classa_net
             ELSE NULL END AS comp2016,
-        CASE WHEN complete_year = '2017'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2017'
+            THEN a.classa_net
             ELSE NULL END AS comp2017,
-        CASE WHEN complete_year = '2018'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2018'
+            THEN a.classa_net
             ELSE NULL END AS comp2018,
-        CASE WHEN complete_year = '2019'
-            THEN classa_net
+        CASE WHEN a.complete_year = '2019'
+            THEN a.classa_net
             ELSE NULL END AS comp2019,
-        CASE WHEN complete_year = '2020' AND date_complete < '2020-07-01'::date
+        CASE WHEN a.complete_year = '2020' AND a.date_complete < '2020-07-01'::date
             THEN classa_net
             ELSE NULL END AS comp2020q2,
-        CASE WHEN job_status = '1. Filed Application'
-                AND job_inactive IS NULL
-            THEN  classa_net 
+        CASE WHEN a.job_status = '1. Filed Application'
+                AND a.job_inactive IS NULL
+            THEN  a.classa_net 
             ELSE NULL END as incmpfiled, 
-        CASE WHEN job_status = '2. Approved Application'
-                AND job_inactive IS NULL
-            THEN  classa_net 
+        CASE WHEN a.job_status = '2. Approved Application'
+                AND a.job_inactive IS NULL
+            THEN  a.classa_net 
             ELSE NULL END as incmpprgrs, 
-        CASE WHEN job_status = '3. Permitted for Construction'
-                AND job_inactive IS NULL
-            THEN  classa_net 
+        CASE WHEN a.job_status = '3. Permitted for Construction'
+                AND a.job_inactive IS NULL
+            THEN  a.classa_net 
             ELSE NULL END as incmprmtd, 
-        CASE WHEN job_status = '9. Withdrawn'
-                AND job_inactive IS NULL
-            THEN  classa_net 
+        CASE WHEN a.job_status = '9. Withdrawn'
+                AND a.job_inactive IS NULL
+            THEN  a.classa_net 
             ELSE NULL END as incmpwtdrn, 
-        CASE WHEN job_status <> '9. Withdrawn'
-                AND job_inactive = 'Inactive'
-            THEN  classa_net 
+        CASE WHEN a.job_status <> '9. Withdrawn'
+                AND a.job_inactive = 'Inactive'
+            THEN  a.classa_net 
             ELSE NULL END as inactive
 INTO YEARLY_devdb
-FROM FINAL_devdb;
+FROM FINAL_devdb a
+JOIN LOOKUP_fips b
+ON a.boro = b.boro;
