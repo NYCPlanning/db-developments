26e000cccff0c3db94744cbf8fbcae23b3ea76c1
mgraber <molly.graber@me.com>
2020-07-14 15:03:26 +0000

    Output QAQC checks in new order

--- developments_build/sql/qaqc/qaqc_final.sql
+++ developments_build/sql/qaqc/qaqc_final.sql
@@ -5,12 +5,16 @@ Alphabetize QAQC table for export
 DROP TABLE IF EXISTS FINAL_qaqc;
 SELECT  
     a.job_number,
-    a.b_large_alt_reduction,
     a.b_likely_occ_desc,
+    a.b_large_alt_reduction,
     a.b_nonres_with_units,
+    a.units_co_prop_mismatch,
+    a.units_init_null,
+    a.units_prop_null,
     a.dem_nb_overlap,
-    a.dup_bbl_address,
     a.dup_bbl_address_units,
+    a.units_res_accessory,
+    a.dup_bbl_address,
     b.geo_water,
     b.geo_taxlot,
     b.geo_null_latlong,
@@ -24,10 +28,6 @@ SELECT
     a.outlier_demo_20plus,
     a.outlier_nb_500plus,
     a.outlier_top_alt_increase,
-    a.units_co_prop_mismatch,
-    a.units_init_null,
-    a.units_prop_null,
-    a.units_res_accessory,
     a.z_inactive_with_update,
     a.z_incomp_tract_home
 INTO FINAL_qaqc
