9912b3862eebea9132b10351eb5c7a61c8e0388e
sptkl <caobaiyue@gmail.com>
2021-02-22 18:19:12 +0000

    [build] and incorporate date permittd corrections
    Former-commit-id: dc2050c9b0e1b41204bed1ab20bef951769f60a6

--- developments_build/sql/_status_q.sql
+++ developments_build/sql/_status_q.sql
@@ -26,9 +26,8 @@ OUTPUTS:
     )
 
 */
-DROP TABLE IF EXISTS STATUS_Q_devdb;
-WITH 
-STATUS_Q_create as (
+DROP TABLE IF EXISTS _STATUS_Q_devdb;
+CREATE TABLE _STATUS_Q_devdb as (
     SELECT 
         jobnum as job_number, 
         min(issuancedate::date) as date_permittd
@@ -36,7 +35,42 @@ STATUS_Q_create as (
     WHERE jobdocnum = '01'
     AND jobtype ~* 'A1|DM|NB'
     GROUP BY jobnum
-) 
+);
+
+/*
+CORRECTIONS: 
+    date_permittd
+*/
+WITH CORR_target as (
+	SELECT a.job_number, 
+		COALESCE(b.reason, 'NA') as reason,
+		b.edited_date
+	FROM _STATUS_Q_devdb a, housing_input_research b
+	WHERE a.job_number=b.job_number
+	AND b.field = 'date_permittd'
+	AND (a.date_permittd::date = b.old_value::date
+		OR (a.date_permittd IS NULL 
+			AND b.old_value IS NULL))
+)
+UPDATE CORR_devdb a
+SET dcpeditfields = array_append(dcpeditfields,'date_permittd')
+FROM CORR_target b
+WHERE a.job_number=b.job_number;
+
+UPDATE _STATUS_Q_devdb a
+SET date_permittd = b.new_value::date
+FROM housing_input_research b
+WHERE a.job_number=b.job_number
+AND b.field = 'date_permittd'
+AND a.job_number in (
+	SELECT DISTINCT job_number 
+	FROM CORR_devdb
+	WHERE 'date_permittd'=any(dcpeditfields));
+
+/*
+COMPUTE year_permit, quarter_permit
+*/
+DROP TABLE IF EXISTS STATUS_Q_devdb;
 SELECT 
     a.job_number,
     b.date_permittd,
@@ -46,5 +80,5 @@ SELECT
     year_quarter(b.date_permittd) as permit_qrtr
 INTO STATUS_Q_devdb
 FROM INIT_devdb a
-LEFT JOIN STATUS_Q_create b
-ON a.job_number = b.job_number
+LEFT JOIN _STATUS_Q_devdb b
+ON a.job_number = b.job_number;
