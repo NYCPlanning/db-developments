b957e65d66064ae7bcd05163627007a58a6e210e
SPTKL <caobaiyue@gmail.com>
2021-07-16 13:35:31 -0400

    455 qaqc mid rewrite (#457)
    * rewrite qaqc_mid to make it faster hopefully
    
    * [build] with rewritten qaqc_mid
    
    * [build] correct typo
    
    * [skip] more typo adding INTO mid_qaqc

--- sql/qaqc/qaqc_mid.sql
+++ sql/qaqc/qaqc_mid.sql
@@ -17,7 +17,8 @@
 **/
 
 DROP TABLE IF EXISTS MATCH_dem_nb;
-SELECT a.job_number as job_number_dem, 
+SELECT 
+	a.job_number as job_number_dem, 
 	b.job_number as job_number_nb,
 	a.geo_bbl
 INTO MATCH_dem_nb
@@ -27,7 +28,8 @@ ON a.geo_bbl = b.geo_bbl
 WHERE a.job_type = 'Demolition'
 AND b.job_type = 'New Building';
 
-DROP TABLE IF EXISTS MID_qaqc;
+-- JOBNUMBER_duplicates
+DROP TABLE IF EXISTS JOBNUMBER_duplicates;
 WITH
 BBL_ADDRESS_groups AS (
 	SELECT COALESCE(geo_bbl, 'NULL BBL') as geo_bbl, address
@@ -45,76 +47,29 @@ BBL_ADDRESS_UNIT_groups AS (
 	AND classa_net IS NOT NULL
 	GROUP BY COALESCE(geo_bbl, 'NULL BBL'), address, classa_net
 	HAVING COUNT(*) > 1
-),
-JOBNUMBER_duplicates AS(
+)
 SELECT 
 	job_number,
-
 	CASE WHEN geo_bbl||address||classa_net
 		IN (SELECT geo_bbl||address||classa_net 
 			FROM BBL_ADDRESS_UNIT_groups)
 		THEN  COALESCE(geo_bbl, 'NULL BBL')||' : '||address||' : '||classa_net
 		ELSE NULL
 	END as dup_bbl_address_units,
-	
 	CASE WHEN geo_bbl||address
 		IN (SELECT geo_bbl||address 
 			FROM BBL_ADDRESS_groups)
 		THEN COALESCE(geo_bbl, 'NULL BBL')||' : '||address
 		ELSE NULL
 	END as dup_bbl_address
-		
-FROM MID_devdb a
-),
-JOBNUMBER_null_init AS(
-    SELECT job_number
-    FROM MID_devdb
-    WHERE
-    job_type IN ('Demolition' , 'Alteration') 
-    AND resid_flag = 'Residential' 
-    AND classa_init IS NULL
-),
-JOBNUMBER_null_prop AS(
-    SELECT job_number
-    FROM MID_devdb
-    WHERE
-    job_type IN ('New Building' , 'Alteration') 
-    AND resid_flag = 'Residential' 
-    AND classa_prop IS NULL
-), 
-JOBNUMBER_nonres_units AS (
-	SELECT job_number 
-	FROM MID_devdb
-	WHERE (occ_initial !~* 'residential' AND classa_init <> 0 AND classa_init IS NOT NULL)
-	OR (occ_proposed !~* 'residential' AND classa_prop <> 0 AND classa_prop IS NOT NULL)
-),
-JOBNUMBER_accessory AS (
-	SELECT job_number
-	FROM MID_devdb
-	WHERE ((address_numbr LIKE '%GAR%' 
-					OR job_desc ~* 'pool|shed|gazebo|garage')
-			AND (classa_init::numeric IN (1,2) 
-					OR classa_prop::numeric IN (1,2)))
-	OR ((occ_initial LIKE '%(U)%'
-			OR occ_initial LIKE '%(K)%'
-			OR occ_proposed LIKE '%(U)%'
-			OR occ_proposed LIKE '%(K)%')
-		AND (classa_init::numeric > 0 
-			OR classa_prop::numeric > 0))
-),
-JOBNUMBER_b_likely AS (
-    SELECT job_number
-    FROM MID_devdb
-    WHERE occ_initial ~* 'hotel|assisted|incapacitated|restrained'
-	OR occ_proposed ~* 'hotel|assisted|incapacitated|restrained'
-    OR job_desc ~* CONCAT('Hotel|Motel|Boarding|Hostel|Lodge|UG 5|UG5', '|',
-                          'Group 5|Grp 5|Class B|Class ''b''|Class "b"', '|',
-                          'SRO |Single room|Furnished|Rooming unit', '|',
-						  'Dorm |Dorms |Dormitor|Transient|Homeless', '|',
-                          'Shelter|Group quarter|Beds|Convent|Monastery', '|',
-                          'Accommodation|Harassment|CNH|Settlement|Halfway', '|',
-                          'Nursing home|Assisted|Supportive|Sleeping', '|',
-						  'UG3|UG 3|Group 3|Grp 3')
+INTO JOBNUMBER_duplicates
+FROM MID_devdb a;
+
+DROP TABLE IF EXISTS MID_qaqc;
+WITH
+JOBNUMBER_dem_nb AS (
+	SELECT DISTINCT job_number_dem as job_number FROM MATCH_dem_nb UNION
+	SELECT DISTINCT job_number_nb as job_number FROM MATCH_dem_nb
 ),
 JOBNUMBER_co_prop_mismatch AS (
     SELECT job_number, co_latest_certtype
@@ -127,55 +82,71 @@ JOBNUMBER_incomplete_tract AS (
     FROM MID_devdb
     WHERE tracthomes = 'Y'
     AND job_status ~* '1|2|3'
-),
-_MID_qaqc AS (
+)
 SELECT 
-	a.*,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_init) 
-		THEN 1 ELSE 0
-	END) as units_init_null,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_null_prop) 
-		THEN 1 ELSE 0
-	END) as units_prop_null,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_nonres_units) 
-		THEN 1 ELSE 0
-	END) as b_nonres_with_units,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_accessory) 
-		THEN 1 ELSE 0
-	END) as units_res_accessory,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_b_likely) 
-		THEN 1 ELSE 0
-	END) as b_likely_occ_desc,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) 
-		THEN (
-			SELECT b.co_latest_certtype 
-			FROM JOBNUMBER_co_prop_mismatch b
-			where b.job_number=a.job_number
-		)  ELSE NULL
-	END) as units_co_prop_mismatch,
-	(CASE 
-		WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_incomplete_tract) 
-		THEN 1 ELSE 0
-	END) as incomp_tract_home,
-	(CASE 
-		WHEN a.job_number IN (
-			SELECT DISTINCT job_number_dem FROM MATCH_dem_nb UNION 
-			SELECT DISTINCT job_number_nb FROM MATCH_dem_nb
-		) THEN 1
-		ELSE 0
-	END) as dem_nb_overlap
-FROM STATUS_qaqc a)
-SELECT 
-	a.*, 
+	STATUS_qaqc.*,
+	(
+		job_type IN ('Demolition' , 'Alteration') AND 
+		resid_flag = 'Residential' AND 
+		classa_init IS NULL
+	)::integer as units_init_null,
+	(
+		job_type IN ('New Building' , 'Alteration') AND 
+		resid_flag = 'Residential' AND 
+		classa_prop IS NULL
+	)::integer as units_prop_null,
+	(
+		(occ_initial !~* 'residential' AND classa_init <> 0 AND classa_init IS NOT NULL) OR 
+		(occ_proposed !~* 'residential' AND classa_prop <> 0 AND classa_prop IS NOT NULL)
+	)::integer as b_nonres_with_units,
+	(
+		(
+			(
+				address_numbr LIKE '%GAR%' OR 
+				job_desc ~* 'pool|shed|gazebo|garage'
+			) AND (
+				classa_init::numeric IN (1,2) OR 
+				classa_prop::numeric IN (1,2)
+			)
+		) OR 
+		(
+			(
+				occ_initial LIKE '%(U)%' OR 
+				occ_initial LIKE '%(K)%' OR 
+				occ_proposed LIKE '%(U)%' OR 
+				occ_proposed LIKE '%(K)%'
+			) AND 
+			(
+				classa_init::numeric > 0 OR 
+				classa_prop::numeric > 0
+			)
+		)
+	)::integer as units_res_accessory,
+	(
+		occ_initial ~* 'hotel|assisted|incapacitated|restrained' OR 
+		occ_proposed ~* 'hotel|assisted|incapacitated|restrained' OR 
+		job_desc ~* CONCAT('Hotel|Motel|Boarding|Hostel|Lodge|UG 5|UG5', '|',
+                          'Group 5|Grp 5|Class B|Class ''b''|Class "b"', '|',
+                          'SRO |Single room|Furnished|Rooming unit', '|',
+						  'Dorm |Dorms |Dormitor|Transient|Homeless', '|',
+                          'Shelter|Group quarter|Beds|Convent|Monastery', '|',
+                          'Accommodation|Harassment|CNH|Settlement|Halfway', '|',
+                          'Nursing home|Assisted|Supportive|Sleeping', '|',
+						  'UG3|UG 3|Group 3|Grp 3')
+	)::integer as b_likely_occ_desc,
+	(
+		CASE WHEN a.job_number IN (SELECT job_number FROM JOBNUMBER_co_prop_mismatch) 
+		THEN c.co_latest_certtype END
+	) as units_co_prop_mismatch,
+	(
+		tracthomes = 'Y' AND 
+		job_status ~* '1|2|3'
+	)::integer as incomp_tract_home,
+	(a.job_number IN (SELECT job_number from JOBNUMBER_dem_nb))::integer as dem_nb_overlap,
 	b.dup_bbl_address, 
 	b.dup_bbl_address_units
 INTO MID_qaqc
-FROM _MID_qaqc a
-JOIN JOBNUMBER_duplicates b 
-ON a.job_number = b.job_number;
+FROM MID_devdb a
+	RIGHT JOIN STATUS_qaqc on a.job_number = STATUS_qaqc.job_number 
+	LEFT JOIN JOBNUMBER_duplicates b ON a.job_number = b.job_number 
+	LEFT JOIN JOBNUMBER_co_prop_mismatch c ON a.job_number = c.job_number;
