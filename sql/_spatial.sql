2f1d88be19ba7d58f9710ef563184a90b2d511ac
sptkl <caobaiyue@gmail.com>
2021-09-22 16:27:58 -0400

    split out query to improve performance

--- sql/_spatial.sql
+++ sql/_spatial.sql
@@ -15,130 +15,130 @@ OUTPUTS:
         same schema as GEO_devdb
     )
 */
-DROP TABLE IF EXISTS SPATIAL_devdb;
-WITH DRAFT_spatial as (
-SELECT
-    distinct
-    a.uid,
+CREATE TEMP TABLE DRAFT_spatial AS (
+    SELECT
+        distinct
+        a.uid,
 
-    -- geo_bbl
-    (CASE WHEN a.geo_bbl IS NULL
-        OR a.geo_bbl ~ '^0' OR a.geo_bbl = ''
-        OR a.mode = 'tpad'
-        THEN get_bbl(geom)
-    ELSE a.geo_bbl END) as geo_bbl, 
-
-    -- geo_bin
-    (CASE WHEN (CASE WHEN a.geo_bbl IS NULL
-        OR a.geo_bbl ~ '^0' OR a.geo_bbl = ''
-        OR a.mode = 'tpad'
-        THEN get_bbl(geom)
-    ELSE a.geo_bbl END) = get_base_bbl(geom)
-        AND (a.geo_bin IS NULL 
-            OR a.geo_bin = '' 
-            OR a.geo_bin::NUMERIC%1000000=0)
+        -- geo_bbl
+        (CASE WHEN a.geo_bbl IS NULL
+            OR a.geo_bbl ~ '^0' OR a.geo_bbl = ''
             OR a.mode = 'tpad'
-        AND get_base_bbl(geom) IS NOT NULL
-        THEN get_bin(geom)
-    ELSE a.geo_bin END) as geo_bin,
+            THEN get_bbl(geom)
+        ELSE a.geo_bbl END) as geo_bbl, 
 
-    a.geo_address_numbr,
-    a.geo_address_street,
-    a.geo_address,
+        -- geo_bin
+        (CASE WHEN (CASE WHEN a.geo_bbl IS NULL
+            OR a.geo_bbl ~ '^0' OR a.geo_bbl = ''
+            OR a.mode = 'tpad'
+            THEN get_bbl(geom)
+        ELSE a.geo_bbl END) = get_base_bbl(geom)
+            AND (a.geo_bin IS NULL 
+                OR a.geo_bin = '' 
+                OR a.geo_bin::NUMERIC%1000000=0)
+                OR a.mode = 'tpad'
+            AND get_base_bbl(geom) IS NOT NULL
+            THEN get_bin(geom)
+        ELSE a.geo_bin END) as geo_bin,
 
-    -- geo_zipcode
-    (CASE WHEN a.geo_zipcode IS NULL 
-        OR a.geo_zipcode = ''
-        OR a.mode = 'tpad'
-        THEN get_zipcode(geom)
-    ELSE a.geo_zipcode END) as geo_zipcode, 
+        a.geo_address_numbr,
+        a.geo_address_street,
+        a.geo_address,
 
-    -- geo_boro
-    (CASE WHEN a.geo_boro IS NULL 
-        OR a.geo_boro = '0'
-        OR a.mode = 'tpad'
-        THEN get_boro(geom)::text
-    ELSE a.geo_boro END) as geo_boro,
+        -- geo_zipcode
+        (CASE WHEN a.geo_zipcode IS NULL 
+            OR a.geo_zipcode = ''
+            OR a.mode = 'tpad'
+            THEN get_zipcode(geom)
+        ELSE a.geo_zipcode END) as geo_zipcode, 
 
-    -- geo_cb2010
-    (CASE WHEN a.geo_cb2010 IS NULL 
-		OR a.geo_cb2010 = '' 
-		OR a.geo_ct2010 = '000000' 
-        OR a.mode = 'tpad'
-        THEN get_cb2010(geom)
-    ELSE a.geo_cb2010 END) as _geo_cb2010, 
+        -- geo_boro
+        (CASE WHEN a.geo_boro IS NULL 
+            OR a.geo_boro = '0'
+            OR a.mode = 'tpad'
+            THEN get_boro(geom)::text
+        ELSE a.geo_boro END) as geo_boro,
 
-    -- geo_ct2010
-   (CASE WHEN a.geo_ct2010 IS NULL 
-		OR a.geo_ct2010 = '' 
-		OR a.geo_ct2010 = '000000'
-        OR a.mode = 'tpad' 
-        THEN get_ct2010(geom)
-    ELSE a.geo_ct2010 END) as _geo_ct2010, 
+        -- geo_cb2010
+        (CASE WHEN a.geo_cb2010 IS NULL 
+            OR a.geo_cb2010 = '' 
+            OR a.geo_ct2010 = '000000' 
+            OR a.mode = 'tpad'
+            THEN get_cb2010(geom)
+        ELSE a.geo_cb2010 END) as _geo_cb2010, 
 
-    -- geo_cb2010
-    (CASE WHEN a.geo_cb2020 IS NULL 
-		OR a.geo_cb2020 = '' 
-		OR a.geo_ct2020 = '000000' 
-        OR a.mode = 'tpad'
-        THEN get_cb2020(geom)
-    ELSE a.geo_cb2020 END) as _geo_cb2020, 
+        -- geo_ct2010
+    (CASE WHEN a.geo_ct2010 IS NULL 
+            OR a.geo_ct2010 = '' 
+            OR a.geo_ct2010 = '000000'
+            OR a.mode = 'tpad' 
+            THEN get_ct2010(geom)
+        ELSE a.geo_ct2010 END) as _geo_ct2010, 
 
-    -- geo_ct2010
-   (CASE WHEN a.geo_ct2020 IS NULL 
-		OR a.geo_ct2020 = '' 
-		OR a.geo_ct2020 = '000000'
-        OR a.mode = 'tpad' 
-        THEN get_ct2020(geom)
-    ELSE a.geo_ct2020 END) as _geo_ct2020, 
-   
-    -- geo_csd
-    (CASE WHEN a.geo_csd IS NULL 
-		OR a.geo_csd = '' 
-        OR a.mode = 'tpad'
-        THEN get_csd(geom)
-    ELSE a.geo_csd END) as geo_csd, 
+        -- geo_cb2010
+        (CASE WHEN a.geo_cb2020 IS NULL 
+            OR a.geo_cb2020 = '' 
+            OR a.geo_ct2020 = '000000' 
+            OR a.mode = 'tpad'
+            THEN get_cb2020(geom)
+        ELSE a.geo_cb2020 END) as _geo_cb2020, 
 
-    -- geo_policeprct
-    (CASE WHEN a.geo_policeprct IS NULL 
-		OR a.geo_policeprct = '' 
-        OR a.mode = 'tpad'
-        THEN get_policeprct(geom)
-    ELSE a.geo_policeprct END) as geo_policeprct, 
+        -- geo_ct2010
+    (CASE WHEN a.geo_ct2020 IS NULL 
+            OR a.geo_ct2020 = '' 
+            OR a.geo_ct2020 = '000000'
+            OR a.mode = 'tpad' 
+            THEN get_ct2020(geom)
+        ELSE a.geo_ct2020 END) as _geo_ct2020, 
+    
+        -- geo_csd
+        (CASE WHEN a.geo_csd IS NULL 
+            OR a.geo_csd = '' 
+            OR a.mode = 'tpad'
+            THEN get_csd(geom)
+        ELSE a.geo_csd END) as geo_csd, 
 
-    -- geo_firedivision
-    (CASE WHEN a.geo_firedivision IS NULL 
-		OR a.geo_firedivision = '' 
-        OR a.mode = 'tpad'
-        THEN get_firedivision(geom)
-    ELSE a.geo_firedivision END) as geo_firedivision, 
+        -- geo_policeprct
+        (CASE WHEN a.geo_policeprct IS NULL 
+            OR a.geo_policeprct = '' 
+            OR a.mode = 'tpad'
+            THEN get_policeprct(geom)
+        ELSE a.geo_policeprct END) as geo_policeprct, 
 
-    -- geo_firebattalion
-    (CASE WHEN a.geo_firebattalion IS NULL 
-		OR a.geo_firebattalion = '' 
-        OR a.mode = 'tpad'
-        THEN get_firebattalion(geom)
-    ELSE a.geo_firebattalion END) as geo_firebattalion, 
+        -- geo_firedivision
+        (CASE WHEN a.geo_firedivision IS NULL 
+            OR a.geo_firedivision = '' 
+            OR a.mode = 'tpad'
+            THEN get_firedivision(geom)
+        ELSE a.geo_firedivision END) as geo_firedivision, 
 
-    -- geo_firecompany
-    (CASE WHEN a.geo_firecompany IS NULL 
-		OR a.geo_firecompany = '' 
-        OR a.mode = 'tpad'
-        THEN get_firecompany(geom)
-    ELSE a.geo_firecompany END) as geo_firecompany, 
+        -- geo_firebattalion
+        (CASE WHEN a.geo_firebattalion IS NULL 
+            OR a.geo_firebattalion = '' 
+            OR a.mode = 'tpad'
+            THEN get_firebattalion(geom)
+        ELSE a.geo_firebattalion END) as geo_firebattalion, 
 
-    get_schoolelmntry(geom) as geo_schoolelmntry,
-    get_schoolmiddle(geom) as geo_schoolmiddle,
-    get_schoolsubdist(geom) as geo_schoolsubdist,
-    a.geo_latitude,
-    a.geo_longitude,
-    a.latitude,
-    a.longitude,
-    a.geom,
-    geomsource
-FROM GEO_devdb a
-),
-CENSUS_TRACT_BLOCK as (
+        -- geo_firecompany
+        (CASE WHEN a.geo_firecompany IS NULL 
+            OR a.geo_firecompany = '' 
+            OR a.mode = 'tpad'
+            THEN get_firecompany(geom)
+        ELSE a.geo_firecompany END) as geo_firecompany, 
+
+        get_schoolelmntry(geom) as geo_schoolelmntry,
+        get_schoolmiddle(geom) as geo_schoolmiddle,
+        get_schoolsubdist(geom) as geo_schoolsubdist,
+        a.geo_latitude,
+        a.geo_longitude,
+        a.latitude,
+        a.longitude,
+        a.geom,
+        geomsource
+    FROM GEO_devdb a
+);
+
+CREATE TEMP TABLE CENSUS_TRACT_BLOCK AS (
     SELECT
         distinct uid,
         (CASE
@@ -153,7 +153,9 @@ CENSUS_TRACT_BLOCK as (
         geo_boro||_geo_ct2010 as bct2010,
         geo_boro||_geo_ct2020 as bct2020
     FROM DRAFT_spatial
-)
+);
+
+DROP TABLE IF EXISTS SPATIAL_devdb;
 SELECT
     DRAFT_spatial.*,
     CENSUS_TRACT_BLOCK.fips||DRAFT_spatial._geo_ct2010||DRAFT_spatial._geo_cb2010 as geo_cb2010,
@@ -173,4 +175,4 @@ SELECT
     (SELECT commntydst FROM lookup_geo WHERE CENSUS_TRACT_BLOCK.bct2010 = bct2010 LIMIT 1) as geo_cd
 INTO SPATIAL_devdb
 FROM DRAFT_spatial
-LEFT JOIN CENSUS_TRACT_BLOCK ON DRAFT_spatial.uid = DRAFT_spatial.uid
+LEFT JOIN CENSUS_TRACT_BLOCK ON DRAFT_spatial.uid = DRAFT_spatial.uid;
