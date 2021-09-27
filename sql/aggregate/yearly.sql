0b25ee16cb5b086d98eb736391648d99d907e745
sptkl <caobaiyue@gmail.com>
2021-09-27 15:11:01 -0400

    [skip] refactoring yearly devdb

--- sql/aggregate/yearly.sql
+++ sql/aggregate/yearly.sql
@@ -10,56 +10,61 @@ OUTPUTS:
     YEARLY_devdb
 */
 
-DROP TABLE IF EXISTS YEARLY_devdb;
+DROP TABLE IF EXISTS YEARLY_devdb_{{ decade }};
 SELECT 
-    a.job_number,
-    b.boro,
-    b.borocode,
-    b.bctcb2010,
-    b.fips_boro||b.ctcb2010 as cenblock10,
-    b.bct2010,
-    b.fips_boro||b.ct2010 as centract10,
-    b.nta as nta2010,
-    b.ntaname as ntaname2010,
-    b.commntydst,
-    b.councildst,
-    CASE WHEN a.complete_year = '2010' AND a.date_complete > '2010-03-31'::date
-            AND a.job_inactive IS NULL
-        THEN a.classa_net
+    job_number,
+    boro,
+    boro as borocode,
+    bctcb20{{ decade }},
+    cenblock{{ decade }},
+    bct20{{ decade }},
+    centract{{ decade }},
+    nta20{{ decade }},
+    ntaname20{{ decade }},
+    comunitydist,
+    councildist,
+
+    CASE WHEN complete_year = '2010' AND date_complete > '2010-03-31'::date
+            AND job_inactive IS NULL
+        THEN classa_net
         ELSE NULL END AS comp2010ap,
     
     {%- for year in years %}
-     CASE WHEN a.complete_year = '{{year}}' AND a.job_inactive IS NULL
+     CASE WHEN complete_year = '{{ year }}' AND job_inactive IS NULL
         THEN classa_net
-        ELSE NULL END AS comp{{year}},
+        ELSE NULL END AS comp{{ year }},
     {% endfor %}
 
-    CASE WHEN a.date_complete > '2010-03-31'::date AND a.date_complete < '{{CAPTURE_DATE}}'::date
-            AND a.job_inactive IS NULL
-        THEN a.classa_net
+    CASE WHEN date_complete > '2010-03-31'::date AND date_complete < '{{CAPTURE_DATE}}'::date
+            AND job_inactive IS NULL
+        THEN classa_net
         ELSE NULL END AS since_cen10,
-    CASE WHEN a.job_status = '1. Filed Application'
-            AND a.job_inactive IS NULL
-        THEN  a.classa_net 
+
+    CASE WHEN job_status = '1. Filed Application'
+            AND job_inactive IS NULL
+        THEN  classa_net 
         ELSE NULL END as filed, 
-    CASE WHEN a.job_status = '2. Approved Application'
-            AND a.job_inactive IS NULL
-        THEN  a.classa_net 
+
+    CASE WHEN job_status = '2. Approved Application'
+            AND job_inactive IS NULL
+        THEN  classa_net 
         ELSE NULL END as approved, 
-    CASE WHEN a.job_status = '3. Permitted for Construction'
-            AND a.job_inactive IS NULL
-        THEN  a.classa_net 
+
+    CASE WHEN job_status = '3. Permitted for Construction'
+            AND job_inactive IS NULL
+        THEN  classa_net 
         ELSE NULL END as permitted, 
-    CASE WHEN a.job_status = '9. Withdrawn'
-            AND a.date_lastupdt::date > '2009-12-31'::date
-        THEN  a.classa_net 
+
+    CASE WHEN job_status = '9. Withdrawn'
+            AND date_lastupdt::date > '2009-12-31'::date
+        THEN  classa_net 
         ELSE NULL END as withdrawn, 
-    CASE WHEN a.job_status <> '9. Withdrawn'
-            AND a.job_inactive ~* 'Inactive'
-            AND a.date_lastupdt::date > '2009-12-31'::date
-        THEN  a.classa_net 
+
+    CASE WHEN job_status <> '9. Withdrawn'
+            AND job_inactive ~* 'Inactive'
+            AND date_lastupdt::date > '2009-12-31'::date
+        THEN  classa_net 
         ELSE NULL END as inactive
+
 INTO YEARLY_devdb
-FROM FINAL_devdb a
-RIGHT JOIN LOOKUP_geo b
-ON a.bctcb2010 = b.bctcb2010;
+FROM FINAL_devdb
