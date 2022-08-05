af32a67316b445f229bff719bf4d6710543775e0
Te Du <tedudevelop@gmail.com>
2022-08-05 14:46:01 -0400

    add everything to qaqc shell script for testing

--- bash/03_qaqc.sh
+++ bash/03_qaqc.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 source bash/config.sh
 
+display "Creating QAQC Table for init table"
+psql $BUILD_ENGINE -f sql/qaqc/qaqc_init.sql
+
+display "Creating QAQC Table for units table"
+psql $BUILD_ENGINE -f sql/qaqc/qaqc_units.sql
+
+display "Creating QAQC Table for status"
+psql $BUILD_ENGINE\
+  -v CAPTURE_DATE_PREV=$CAPTURE_DATE_PREV\
+  -f sql/qaqc/qaqc_status.sql
+
+display "Creating QAQC Table for qaqc mid and geo"
+psql $BUILD_ENGINE -f sql/qaqc/qaqc_mid.sql
+psql $BUILD_ENGINE -f sql/qaqc/qaqc_geo.sql
+
 display "Creating QAQC Table for QAQC Application"
 psql $BUILD_ENGINE -f sql/qaqc/qaqc_app_additions.sql
 psql $BUILD_ENGINE -f sql/qaqc/qaqc_app.sql
