903971a79b4b1f691b1bf61e252b614cd27ae72b
Te Du <tedudevelop@gmail.com>
2022-08-09 14:47:20 -0400

    first draft qaqc

--- sql/qaqc/qaqc_app_additions.sql
+++ sql/qaqc/qaqc_app_additions.sql
@@ -15,3 +15,20 @@ SELECT a.job_number,
 	END) as classa_net_mismatch
 INTO qaqc_app_additions
 FROM FINAL_devdb a;
+
+ALTER TABLE qaqc_app_additions ADD manual_hny_match_check INT;
+WITH manual_hny_match_check AS (SELECT 
+job_number
+FROM corr_hny_matches 
+WHERE 
+action = 'add' 
+AND
+hny_id IN (SELECT hny_id FROM hny_no_match)
+)
+UPDATE qaqc_app_additions 
+SET manual_hny_match_check = 1
+FROM qaqc_app_additions q, manual_hny_match_check m
+WHERE q.job_number = m.job_number;
+
+UPDATE qaqc_app_additions
+SET manual_hny_match_check=(CASE WHEN manual_hny_match_check=1 THEN 1 ELSE 0 END);
