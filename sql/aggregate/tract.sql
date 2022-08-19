1a887c3e7f321da467795f7c4890055be3d10676
Te Du <tedudevelop@gmail.com>
2022-08-19 13:31:04 -0400

    remove census units

--- sql/aggregate/tract.sql
+++ sql/aggregate/tract.sql
@@ -18,31 +18,31 @@ SELECT
     SUM(withdrawn) as withdrawn,
     SUM(inactive) as inactive
 
-    {% if decade == '2010' %}
+    -- {% if decade == '2010' %}
 
-        ,SUM(CENSUS_by_tract.cenunits10) as cenunits10
-        ,SUM(COALESCE(YEARLY_devdb_{{ decade }}.since_cen10, 0) + COALESCE(CENSUS_by_tract.cenunits10, 0)) as total
-        ,SUM(census_units10adj.adjunits10) as adjunits10
-        ,SUM(COALESCE(YEARLY_devdb_{{ decade }}.since_cen10, 0) + COALESCE(census_units10adj.adjunits10, 0)) as totaladj
+    --     ,SUM(CENSUS_by_tract.cenunits10) as cenunits10
+    --     ,SUM(COALESCE(YEARLY_devdb_{{ decade }}.since_cen10, 0) + COALESCE(CENSUS_by_tract.cenunits10, 0)) as total
+    --     ,SUM(census_units10adj.adjunits10) as adjunits10
+    --     ,SUM(COALESCE(YEARLY_devdb_{{ decade }}.since_cen10, 0) + COALESCE(census_units10adj.adjunits10, 0)) as totaladj
     
-    {% endif %}
+    -- {% endif %}
 
 
 INTO AGGREGATE_tract_{{ decade }}
 FROM YEARLY_devdb_{{ decade }}
 
-{% if decade == '2010' %}
+-- {% if decade == '2010' %}
 
-    LEFT JOIN (
-        SELECT centract10, SUM(cenunits10) as cenunits10
-        FROM census_units10
-        GROUP BY centract10
-    ) CENSUS_by_tract 
-        ON YEARLY_devdb_{{ decade }}.centract2010 = CENSUS_by_tract.centract10
-    LEFT JOIN census_units10adj 
-        ON YEARLY_devdb_{{ decade }}.centract2010 = census_units10adj.centract10
+--     LEFT JOIN (
+--         SELECT centract10, SUM(cenunits10) as cenunits10
+--         FROM census_units10
+--         GROUP BY centract10
+--     ) CENSUS_by_tract 
+--         ON YEARLY_devdb_{{ decade }}.centract2010 = CENSUS_by_tract.centract10
+--     LEFT JOIN census_units10adj 
+--         ON YEARLY_devdb_{{ decade }}.centract2010 = census_units10adj.centract10
 
-{% endif %}
+-- {% endif %}
 
 GROUP BY
     YEARLY_devdb_{{ decade }}.boro,
