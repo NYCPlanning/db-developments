5b5bbc16d0a3e8ad694893ed0fac0982f791c10b
Te Du <tedudevelop@gmail.com>
2022-09-12 14:17:27 -0400

    create lookup dicts

--- python/clean_export_aggregate.py
+++ python/clean_export_aggregate.py
@@ -3,12 +3,12 @@ import sys
 from sqlalchemy import create_engine
 
 template_lookup = {
-    "CDTA": "cdta2020",
-    "CensusBlocks": "",
-    "CensusTracts": "",
-    "CityCouncil": "",
-    "CommunityDistricts": "",
-    "NTA": "",
+    "aggregate_cdta_2020": "CDTA",
+    "aggregate_block_2020": "CensusBlocks",
+    "aggregate_tract_2020": "CensusTracts",
+    "aggregate_councildst_2010": "CityCouncil",
+    "aggregate_commntydst_2010": "CommunityDistricts",
+    "aggregate_nta_2020": "NTA",
 }
 
 BUILD_ENGINE = sys.argv[1]
@@ -16,16 +16,30 @@ BUILD_ENGINE = sys.argv[1]
 
 def read_aggregate_template(name: str):
     base = pd.read_csv(f"data/agg_template/{name}.csv")
-    base.set_index(template_lookup[name], inplace=True)
+    base.set_index(get_index_columns(name), inplace=True)
     return base
+
+
+def get_index_columns(name: str):
+    index_name = {
+        "aggregate_cdta_2020": "cdta2020",
+        "aggregate_block_2020": "bctcb2020",
+        "aggregate_tract_2020": "bct2020",
+        "aggregate_councildst_2010": "councildst",
+        "aggregate_commntydst_2010": "commntydist",
+        "aggregate_nta_2020": "nta2020",
+    }
+    return index_name[name]
 
 
 if __name__ == "__main__":
 
     table = sys.argv[2]
-    engine = create_engine(BUILD_ENGINE)
 
+    engine = create_engine(BUILD_ENGINE)
     base = read_aggregate_template(table)
-    df = pd.read_sql("""SELECT * FROM """, con=engine)
+    df = pd.read_sql(f"""SELECT * FROM {table}""", con=engine)
 
     final = pd.concat([base, df], axis=1)
+
+    final.to_csv(f"{table}.csv", index=False)
