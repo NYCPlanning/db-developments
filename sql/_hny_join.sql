7e8b040360e93b8c090248d80c2a614fa52194a3
Te Du <tedudevelop@gmail.com>
2022-09-19 12:05:18 -0400

    docstring edits

--- sql/_hny_join.sql
+++ sql/_hny_join.sql
@@ -4,15 +4,12 @@ DESCRIPTION:
     Following _hny.sql query with HNY_matches were completed
 
     5) Assign flags to indicate one_hny_to_many_dev and/or one_dev_to_many_hny.
-    6) Resolve the one-to-many, many-to-one, and many-to-many cases in HNY_matches
-        in order to create HNY_lookup
-        a) One-to-one matches get assigned directly
-        b) For one devdb to many hny, sum the total_units and all_counted_units for all hny rows
-        c) For multiple devdb to one hny, assign units to the one with the lowest job_number.
-            Remaining matches are retained, but get NULLs in the unit fields.
-    7) Merge  devdb with UNION 
-        JOIN KEY: job_number
+        a) handle one devdb to many hny records cases by concatenating those hny_ids and combines units/fields 
+        to create a single record
+        b) Rest of the cases (one hny to many devdb, many hny to many devdb) are handled by including every 
+        possible joins with devdb records for each hny recor.
 
+    6) Combine the two tables to create HNY_devdb
 
 INPUTS: 
     HNY_matches (
@@ -123,8 +120,8 @@ WITH
 		END) AS one_dev_to_many_hny
     FROM HNY_matches m), --- maybe this is where the hny_geo could be brought in again
 
--- 6) ASSIGN MATCHES   
-	-- b) For one dev to many hny, group by job_number and sum unit fields
+-- 5) ASSIGN MATCHES   
+	-- a) For one dev to many hny, group by job_number and sum unit fields
 	one_to_many AS (SELECT 
         string_agg(r.hny_id, ', ') AS hny_id,
         r.job_number, 
@@ -156,42 +153,43 @@ WITH
         ON r.hny_id = h.hny_id
         WHERE r.one_dev_to_many_hny = 1 AND r.one_hny_to_many_dev = 0
         GROUP BY r.job_number, r.one_dev_to_many_hny, r.one_hny_to_many_dev), 
-    -- this would include all other hny devdb relationship 
- other_relations AS (
-        SELECT
-            r.hny_id,
-            r.job_number::TEXT as job_number,
-            r.all_counted_units::text as classa_hnyaff,
-            r.total_units::text as all_hny_units,
-            r.one_dev_to_many_hny,
-            r.one_hny_to_many_dev,
-            h.project_start_date as project_start_date,
-            h.project_completion_date as project_completion_date,
-            h.extremely_low_income_units::NUMERIC as extremely_low_income_units,
-            h.very_low_income_units::NUMERIC as very_low_income_units,
-            h.low_income_units::NUMERIC as low_income_units,
-            h.moderate_income_units::NUMERIC as moderate_income_units,
-            h.middle_income_units::NUMERIC as middle_income_units,
-            h.other_income_units::NUMERIC as other_income_units,
-            h.studio_units::NUMERIC as studio_units,
-            h."1_br_units"::NUMERIC as "1_br_units",
-            h."2_br_units"::NUMERIC as "2_br_units",
-            h."3_br_units"::NUMERIC as "3_br_units",
-            h."4_br_units"::NUMERIC as "4_br_units",
-            h."5_br_units"::NUMERIC as "5_br_units",
-            h."6_br+_units"::NUMERIC as "6_br+_units",
-            h.unknown_br_units::NUMERIC as unknown_br_units,
-            h.counted_rental_units::NUMERIC as counted_rental_units,
-            h.counted_homeownership_units::NUMERIC as counted_homeownership_units
-        FROM RELATEFLAGS_hny_matches r
-        LEFT JOIN HNY_geo h
-        ON r.hny_id = h.hny_id
-        WHERE NOT (r.one_dev_to_many_hny = 1 AND r.one_hny_to_many_dev = 0))
-    
--- 7) MERGE WITH devdb  
+    -- b) this would include all other hny devdb relationship 
+    other_relations AS (
+            SELECT
+                r.hny_id,
+                r.job_number::TEXT as job_number,
+                r.all_counted_units::text as classa_hnyaff,
+                r.total_units::text as all_hny_units,
+                r.one_dev_to_many_hny,
+                r.one_hny_to_many_dev,
+                h.project_start_date as project_start_date,
+                h.project_completion_date as project_completion_date,
+                h.extremely_low_income_units::NUMERIC as extremely_low_income_units,
+                h.very_low_income_units::NUMERIC as very_low_income_units,
+                h.low_income_units::NUMERIC as low_income_units,
+                h.moderate_income_units::NUMERIC as moderate_income_units,
+                h.middle_income_units::NUMERIC as middle_income_units,
+                h.other_income_units::NUMERIC as other_income_units,
+                h.studio_units::NUMERIC as studio_units,
+                h."1_br_units"::NUMERIC as "1_br_units",
+                h."2_br_units"::NUMERIC as "2_br_units",
+                h."3_br_units"::NUMERIC as "3_br_units",
+                h."4_br_units"::NUMERIC as "4_br_units",
+                h."5_br_units"::NUMERIC as "5_br_units",
+                h."6_br+_units"::NUMERIC as "6_br+_units",
+                h.unknown_br_units::NUMERIC as unknown_br_units,
+                h.counted_rental_units::NUMERIC as counted_rental_units,
+                h.counted_homeownership_units::NUMERIC as counted_homeownership_units
+            FROM RELATEFLAGS_hny_matches r
+            LEFT JOIN HNY_geo h
+            ON r.hny_id = h.hny_id
+            WHERE NOT (r.one_dev_to_many_hny = 1 AND r.one_hny_to_many_dev = 0))
+        
+-- 6) Insert into HNY_devdb  
 SELECT 
 *
-INTO HNY_devdb
+INTO 
+HNY_devdb
 FROM (SELECT * FROM one_to_many
     UNION
     SELECT * FROM other_relations) all_hny;
