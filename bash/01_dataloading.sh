6dfbc245f1ca357e47eafdb72d327c98d7a7a7ac
mbh329 <mbrueckner@planning.nyc.gov>
2022-11-17 10:38:14 -0500

    remove versions after successful run of action with shapefile outputs

--- bash/01_dataloading.sh
+++ bash/01_dataloading.sh
@@ -5,7 +5,7 @@ source bash/config.sh
 MODE="${1:-edm}"
 
 max_bg_procs 5
-import_public dob_now_applications 20221001 &
+import_public dob_now_applications &
 import_public dob_now_permits &
 import_public council_members &
 import_public doe_school_subdistricts &
